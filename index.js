820786b5af1dd55ee091f410e1e12c984ac6e483
eight <eight04@gmail.com>
2018-06-25 21:32:10 +0800

    Add: new API

--- index.js
+++ index.js
@@ -10,16 +10,49 @@ function tryAccess(file) {
 
 function tryRequire(file) {
   return requireAsync(file)
-    .catch(() => false);
+    .catch(() => null);
 }
 
-function createConfigLocator({_tryRequire = tryRequire, _tryAccess = tryAccess} = {}) {
+const DEFAULT_EXTENSIONS = {
+  ".js": tryRequire,
+  ".json": tryRequire
+};
+
+function createConfigLocator({
+  config,
+  findAll = false,
+  stopAtRoot = true,
+  stopAt = null,
+  extensions = null,
+  _tryAccess = tryAccess
+} = {}) {
   const cache = new Map;
-  return {findConfig};
+  extensions = Object.assign({}, DEFAULT_EXTENSIONS, extensions);
+  config = (Array.isArray(config) ? config : [config])
+    .map(filename => {
+      const loader = extensions[path.extname(filename)];
+      return {filename, loader};
+    });
+  return {findConfig, searchDir, clearCache};
+  
+  function clearCache() {
+    cache.clear();
+  }
   
   function findConfig(file) {
     let dir = path.dirname(path.resolve(file));
     return searchDir(dir);
+  }
+  
+  function waitAll(promises) {
+    return Promise.all(promises)
+      .then(result => result.filter(Boolean));
+  }
+  
+  function getFirst(promises) {
+    return promises.reduce((pending, curr) =>
+      pending.then(value => value ? value : curr)
+    );
   }
   
   function searchDir(dir) {
@@ -27,23 +60,27 @@ function createConfigLocator({_tryRequire = tryRequire, _tryAccess = tryAccess} 
     if (cachedResult) {
       return cachedResult;
     }
-    const confPath = path.join(dir, ".inline.js");
-    const pkgPath = path.join(dir, "package.json");
-    const pendingPkg = _tryAccess(pkgPath);
-    const pending = _tryRequire(confPath)
-      .then(conf => {
-        if (conf) {
-          return {conf, confPath};
-        }
-        return pendingPkg.then(pkg => {
-          if (!pkg) {
+    const pendingRoot = stopAtRoot ? _tryAccess(path.join(dir, "package.json")) : Promise.resolve(false);
+    const pendingStop = stopAt ? stopAt(dir, pendingConfig) : Promise.resolve(false);
+    const pendingConfig = (findAll ? waitAll : getFirst)(config.map(({filename, loader}) => {
+      filename = path.join(dir, filename);
+      return loader(filename)
+        .then(obj => obj ? {filename, config: obj} : null);
+    }));
+    const pending = Promise.all([pendingRoot, pendingStop])
+      .then(shouldStop => {
+        shouldStop = shouldStop.reduce((a, b) => a || b);
+        return pendingConfig
+          .then(config => {
             const parentDir = path.dirname(dir);
-            if (parentDir !== dir) {
-              return searchDir(parentDir);
+            if (config && !findAll || shouldStop || parentDir === dir) {
+              return config;
             }
-          }
-          return null;
-        });
+            return searchDir(parentDir)
+              .then(parentConfig =>
+                findAll ? config.concat(parentConfig) : parentConfig
+              );
+          });
       });
     cache.set(dir, pending);
     return pending;
@@ -51,7 +88,7 @@ function createConfigLocator({_tryRequire = tryRequire, _tryAccess = tryAccess} 
 }
 
 module.exports = {
-  findConfig: file => createConfigLocator().findConfig(file),
+  findConfig: (file, options) => createConfigLocator(options).findConfig(file),
   createConfigLocator,
   tryRequire,
   tryAccess
